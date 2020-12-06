70bcabff2d244ed01ebbb552057034364f70cd5c
RusAnonym <alexandrsemin2033@gmail.com>
2020-12-06 17:09:18 +0300

    fix getTariff

--- src/api/getTariff.ts
+++ src/api/getTariff.ts
@@ -7,6 +7,8 @@ import { callMethod } from "../lib/core";
 export default async function getTariff(
 	params: GetTariffRequestParams,
 ): Promise<GetTariffResponseParams> {
+	params.driveTariffIds = params.driveTariffIds || [];
+	params.tariffIds = params.tariffIds || [];
 	return await callMethod("tariff", {
 		sessionToken: params.sessionToken,
 		tariffIds: params.tariffIds.join(),
