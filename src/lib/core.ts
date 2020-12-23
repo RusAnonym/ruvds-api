7bfbd8f31e2aec74f2a98e114270714a82702f0c
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-23 06:56:41 +0300

    small update & add .npmignore

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -3,12 +3,19 @@ import axios from "axios";
 const API_URL = "https://ruvds.com/api/";
 
 async function callMethod(method: string, params: any) {
-	return await (
+	let outputData = await (
 		await axios({
 			url: API_URL + method,
 			params: params,
 		})
 	).data;
+	if (outputData.errMessage) {
+		throw new Error(
+			`Code: ${outputData.rejectReason} - ${outputData.errMessage}`,
+		);
+	} else {
+		return outputData;
+	}
 }
 
 export { callMethod };
